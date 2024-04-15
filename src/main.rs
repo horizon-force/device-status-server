b437e35e1b8826ee4786b6ebe50a7037f5a2f4d1
Deepak Ramalingam <rdeepak2002@gmail.com>
2024-04-15 08:00:48 -0700

    Starter code
    Took 13 minutes

---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+use std::env;
+use tracing_subscriber::fmt::format;
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        // `POST /users` goes to `create_user`
+        .route("/users", post(create_user));
+
+    // run our app with hyper, listening globally on port 8081
+    let port = env::var("PORT")
+        .unwrap_or("8081".parse().unwrap())
+        .parse::<u16>()
+        .expect("Invalid PORT number");
+    let listener = tokio::net::TcpListener::bind(format!("{}{}", "0.0.0.0", format!(":{}", port)))
+        .await
+        .unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<CreateUser>,
+) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
